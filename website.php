384572c01cf004bc426ad6ce8983daaf323597bf
Andy Muns <andymuns@Andy-Muns-MBP.local>
2017-09-26 14:35:06 -0700

    version 2

---
+++ website.php
@@ -0,0 +1,13 @@
+<?php include 'variables/global.php';?>
+<?php include 'variables/page.php';?>
+<head>
+<meta charset='utf-8'>
+<meta http-equiv='x-ua-compatible' content='ie=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<?php include 'incl/seo.php';?>
+<?php include 'incl/social.php';?>
+<script type='application/ld+json'>
+<?php include 'json-ld/website.php';?>
+</script>
+</head>
+
